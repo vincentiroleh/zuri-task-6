0d076a241ef5d6658673c3dbe1d5a8c8963d5d82
Vincent Iroleh <irolehiroleh@gmail.com>
2021-05-10 01:16:10 +0100

    handle error

--- app.js
+++ app.js
@@ -18,11 +18,27 @@ app.use(express.json()); // bodyParser
 app.use(logger('dev')); // morgan logger
 app.use(`${api}`, require('./api/index')); // routers
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.status(400).json({
-        message: 'Resource in search not available, check our doc',
+        message: 'Welcome on board',
         doc: 'https://github.com/vincentiroleh/zuri-task-6'
     })
+});
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || 'Internal Server Error',
+        },
+    });
 });
 
 module.exports = app;
